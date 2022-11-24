4c250828cca8a2395d0949a1b20cfa2ed7bbfc8c
rahandi <rahandinoor@gmail.com>
2022-11-24 17:45:42 +0900

    wip

--- app.py
+++ app.py
@@ -7,4 +7,18 @@ app = Flask(__name__)
 
 @app.route('/registration', methods=['GET'])
 def registration():
-    return redirect(KRATOS_URL + "/self-service/registration/browser")
+    args = request.args
+    flowId = args.get('flow')
+
+    if flowId:
+        return flowId
+
+    return redirect(KRATOS_URL + "/self-service/registration/api")
+
+@app.route('/registration', methods=['POST'])
+def registration_post():
+    body = request.get_json()
+    username = body.get('username')
+    password = body.get('password')
+
+    return username + password
